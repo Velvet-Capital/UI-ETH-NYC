c920842381d70fd0f8d35746807ba155ac81ee73
raman baral <ramanisstudent@gmail.com>
2022-05-19 17:03:32 +0545

    downloaded icons

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import './Header.css';
+import '../../styles/utils.css'
+
+import GhostLogo from '../../assets/img/ghost-logo.png';
+import Logo from '../../assets/img/headerlogo.png';
+import WalletImg from '../../assets/img/wallet.png';
+
+function Header({toggleConnectWalletModal, isWalletConnected}) {
+    return (
+        <div className="header">
+            <img src={GhostLogo} alt="" id="ghost-logo" draggable="false" />
+            <img src={Logo} alt="" id="header-logo" draggable="false" />
+
+            {isWalletConnected ? (
+            <div className="header-investor-data">
+                <div>
+                    <span className="header-investor-data-title fn-sm">Balance</span>
+                    <span className="balance fn-lg">$15,000</span>
+                </div>
+
+                <div>
+                    <span className="header-investor-data-title fn-sm">Return</span>
+                    <span className="return fn-lg c-green">+ $5,000 (20%)</span>
+                </div>
+            </div> ) : null}
+
+            {!isWalletConnected ? (
+                <button className="connect-btn" onClick={toggleConnectWalletModal}>
+                    <img src={WalletImg} alt="" />
+                    <span className="fn-sm">Connect a wallet</span>
+                </button>
+            ) : (
+                <button className="connect-btn" >
+                    <img src={WalletImg} alt="" />
+                    <span className="fn-sm">0x19..709</span>
+                </button> 
+            )}
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './Modal.css';
+
+import VelvetImg from '../../assets/img/velvet.svg';
+import MetamaskImg from '../../assets/img/metamask.webp';
+
+function Modal(props) {
+    if(!props.show) return null;
+
+    return (
+        <>
+            <div className="overlay" onClick={props.toggleConnectWalletModal} >
+            </div>
+            <div className='modal show'>
+                <img src={VelvetImg} alt="" className='connect-modal-velvet-logo horizontally-centred'/>
+                <h2 className="connect-modal-title">Create wallet</h2>
+                <input type="email" name="email" placeholder="E-mail" id="connect-modal-input" />
+                <button className="btn fn-md connect-modal-signup-btn">Sign Up</button>
+                <p className="fn-sm" style={{textAlign: 'center', color: '#564dd0', cursor: 'pointer', fontWeight: 500}}>I already have an account</p>
+                <h2 className="connect-modal-title my-30">Connect wallet</h2>
+                <button className="connect-modal-metamask-btn fn-md">
+                    <span> Connect with Metamask</span>
+                    <img src={MetamaskImg} alt="" />
+                </button>
+                <p className="text-center fn-vsm">By connecting you are agree to our <b>Terms of use and Privacy Policy</b></p>
+            </div>
+        </>
+    )
+}
+
+export default Modal;
