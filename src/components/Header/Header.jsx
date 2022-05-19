7ba4037b504a44eb8eb6079cc9888841982c7445
raman baral <ramanisstudent@gmail.com>
2022-05-19 22:17:22 +0545

    created header connect wallet btn dropdown with animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './Header.css';
 import '../../styles/utils.css'
 
@@ -7,14 +7,25 @@ import Logo from '../../assets/img/headerlogo.png';
 import WalletImg from '../../assets/img/wallet.png';
 import ArrowUPImg from '../../assets/img/chevron-down (1).svg';
 import ArrowDownImg from '../../assets/img/chevron-down.svg';
+import ExitImg from '../../assets/img/exit.svg';
 
 function Header({toggleConnectWalletModal, isWalletConnected}) {
+
+    const [showDropdownMenu, setShowDropdownMenu] = useState(false);
+
+    function toggleDropdownMenu() {
+        if (showDropdownMenu)
+            setShowDropdownMenu(false);
+        else
+            setShowDropdownMenu(true);
+    }
+
     return (
         <div className="header">
             <img src={GhostLogo} alt="" id="ghost-logo" draggable="false" />
             <img src={Logo} alt="" id="header-logo" draggable="false" />
 
-            {isWalletConnected ? (
+            {isWalletConnected && (
             <div className="header-investor-data">
                 <div>
                     <span className="header-investor-data-title fn-sm">Balance</span>
@@ -25,7 +36,7 @@ function Header({toggleConnectWalletModal, isWalletConnected}) {
                     <span className="header-investor-data-title fn-sm">Return</span>
                     <span className="return fn-lg c-green">+ $5,000 (20%)</span>
                 </div>
-            </div> ) : null}
+            </div> )}
 
             {!isWalletConnected ? (
                 <button className="connect-btn" onClick={toggleConnectWalletModal}>
@@ -33,12 +44,27 @@ function Header({toggleConnectWalletModal, isWalletConnected}) {
                     <span className="fn-sm">Connect a wallet</span>
                 </button>
             ) : (
-                <button className="connect-btn" >
+                <button className="connect-btn" onClick={toggleDropdownMenu}>
                     <img src={WalletImg} alt="" />
                     <span className="fn-sm">0x19..709</span>
-                    <img src={ArrowDownImg} className="connect-btn-icon" alt="" />
+                    <img src={showDropdownMenu ? ArrowUPImg : ArrowDownImg} className="connect-btn-icon" alt="" />
                 </button> 
             )}
+            {
+                showDropdownMenu && (
+                <div className="header-dropdown-menu">
+                    <div>
+                        <p className="fn-vsm">Wallet balance</p>
+                        <span>$10,000</span>
+                    </div>
+                    <hr />
+                    <div>
+                        <span className="fn-sm">Disconnect</span>
+                        <img src={ExitImg} alt="" />
+                    </div>
+                </div> )
+            }
+
         </div>
     )
 }
