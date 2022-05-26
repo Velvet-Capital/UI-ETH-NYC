37c6e870a687bc54277c4207767cc05d50987fb5
raman baral <ramanisstudent@gmail.com>
2022-05-26 08:23:47 +0545

    added link to terms and conditions

--- src/components/ConnectModal/ConnectModal.jsx
+++ src/components/ConnectModal/ConnectModal.jsx
@@ -31,7 +31,7 @@ function Modal(props) {
                     <img src={MetamaskImg} alt="" />
                 </button>
 
-                <p className="text-center fn-vsm">By connecting you are agree to our <b>Terms of use and Privacy Policy</b></p>
+                <p className="text-center fn-vsm">By connecting you agree to our <a href="https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:54249449-1664-4b64-a11e-feb9dbcbf507" target="_blank"><b>Terms of use and Privacy Policy</b></a></p>
             </div>
         </>
     )
