8a05a3cee96c656a0070086b2516bfabd2801edc
raman baral <ramanisstudent@gmail.com>
2022-06-10 20:36:12 +0545

    integrated new meta & top10 portoflio

--- src/App.js
+++ src/App.js
@@ -79,8 +79,8 @@ function App() {
     const [top7IndexTokensWeight, setTop7IndexTokensWeight] = useState({});
     
     const bluechipIndexContractAddressMainnet = '0x0eCc8ed9f1157d85E5e078BDc68B7C98eb8A251A';
-    const metaIndexContractAddressMainnet = '0xB757F1D8c40D49313f716906d7c3107a877367AD';
-    const top10IndexContractAddressMainnet = '0x210b31776fA73c72CCaD41A65AcAF1Ab3317440E';
+    const metaIndexContractAddressMainnet = '0x9F00664f883dE5F67a71cAbA3059b6Caf345cB41';
+    const top10IndexContractAddressMainnet = '0x2C338E6e014B0aC11Bc06E5cb571A2b12d020B39';
     const top10VenusContractAddressMainnet = '0x187b397599d81285a987466bD14790CF779B69E8';
     const top7IndexContractAddressTestnet = '0x5DA92941262768deA5018114e64EB73b937B5Cb0';
     const indexSwapAbi = indexSwap.abi;
