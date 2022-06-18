bbe535295ede1d97e49b3fc0f118f6d4f93ab318
raman baral <ramanisstudent@gmail.com>
2022-06-18 21:21:10 +0545

    added constants.js and stored contract Addresses

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+const bluechipIndexContractAddressMainnet = '0x0eCc8ed9f1157d85E5e078BDc68B7C98eb8A251A';
+const metaIndexContractAddressMainnet = '0x9F00664f883dE5F67a71cAbA3059b6Caf345cB41';
+const top10IndexContractAddressMainnet = '0x2C338E6e014B0aC11Bc06E5cb571A2b12d020B39';
+const top10VenusContractAddressMainnet = '0x187b397599d81285a987466bD14790CF779B69E8';
+const top7IndexContractAddressTestnet = '0x5DA92941262768deA5018114e64EB73b937B5Cb0';
+
+export {bluechipIndexContractAddressMainnet, metaIndexContractAddressMainnet, top10IndexContractAddressMainnet, top10VenusContractAddressMainnet, top7IndexContractAddressTestnet}
